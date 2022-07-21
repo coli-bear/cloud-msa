033059ad4358b81eb04b01974d1a0e795694987c
박건태 <icraft@baggeontaeui-MacBookPro.local>
2022-07-21 18:37:08 +0900

    cloud custom auth filter

--- README.md
+++ README.md
@@ -3,11 +3,24 @@
 - spring cloud 를 이용하여 msa 기본 구성
 - 하나하나 구현해 나가자!
 
-## Config Server - Spring Cloud Config
-https://wonit.tistory.com/503?category=854728
-
 ## Discovery - Netflix Eureka
-https://wonit.tistory.com/495?category=854728
+
+- https://wonit.tistory.com/495?category=854728
 
 ## Gateway - Spring Cloud GateWay
-https://wonit.tistory.com/497?category=854728
+
+- https://wonit.tistory.com/497?category=854728
+- https://wonit.tistory.com/499?category=854728
+- https://wonit.tistory.com/500?category=854728
+
+## Config - Spring Cloud Config
+
+- https://wonit.tistory.com/502?category=854728
+- https://wonit.tistory.com/503?category=854728
+- https://wonit.tistory.com/504?category=854728
+- https://wonit.tistory.com/505?category=854728
+
+## Microservice 간 통신
+
+- https://wonit.tistory.com/506?category=854728
+- 
--- cloud-member/src/main/java/com/example/cloudmember/CloudMemberApplication.java
+++ cloud-member/src/main/java/com/example/cloudmember/CloudMemberApplication.java
@@ -5,8 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 @EnableDiscoveryClient
@@ -19,18 +22,25 @@ public class CloudMemberApplication {
     }
 
 
-    @GetMapping("/members/config")
-    public String config(
+    @GetMapping("/config")
+    public List<String> config(
         @Value("${spring.profiles.active}") String active,
+        @Value("${default.product.names}") List<String> values,
         @Value("${default.message}") String message
     ) {
-//        String active = "TEST";
-        return active.toUpperCase(Locale.ROOT) +"`s default message: " + message;
+//        List<String> values = new ArrayList<>();
+        values.add(active.toUpperCase(Locale.ROOT) +"`s default message: " + message);
+        return values;
     }
 
-    @GetMapping("/members/info")
+    @GetMapping("/info")
     public String info(@Value("${server.port}") String port) {
 
         return "member port : " + port;
+    }
+
+    @GetMapping("/auth")
+    public String auth(@RequestHeader(value = "token") String token) {
+        return "token is " + token;
     }
 }
